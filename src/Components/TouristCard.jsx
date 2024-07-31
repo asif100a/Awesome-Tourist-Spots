d766bbb4a54b0fe03b94c68d154e11f82a571476
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-07-31 22:42:52 +0600

    Tourist cards changed style

--- src/Components/TouristCard.jsx
+++ src/Components/TouristCard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import './touristCard.css';
 
 const TouristCard = ({ spotCard }) => {
     const { _id, img_url, location, spot_name, total_cost } = spotCard;
@@ -7,19 +8,18 @@ const TouristCard = ({ spotCard }) => {
 
     return (
         <div className='relative'>
-           
-            <div className="max-w-xs rounded-md shadow-md hover:shadow-lg transition-transform transform hover:scale-[1.03] dark:bg-gray-50 dark:text-gray-800 mt-6">
-                <img src={img_url} alt="Spot image" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+            <div className="max-w-xs rounded-[22px] shadow-md hover:shadow-lg transition-transform transform hover:scale-[1.03] dark:bg-gray-50 dark:text-gray-800 card-border mt-6">
+                <img src={img_url} alt="Spot image" className="object-cover object-center w-full rounded-t-[22px] h-56 dark:bg-gray-500" />
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide h-28">{spot_name}</h2>
+                        <h2 className="text-xl font-semibold tracking-wide h-16">{spot_name}</h2>
                         <div className='h-14'>
-                            <p className="dark:text-gray-800">{location}</p>
-                            <p className='text-lg font-semibold mt-3'>Tour price: {total_cost}</p>
+                            <p className="dark:text-gray-800 text-sm">{location}</p>
+                            <p className='text-base font-semibold mt-3'>Tour price: <span className='font-normal'>{total_cost}</span></p>
                         </div>
                     </div>
-                    <Link to={`/tourist_card_details/${_id}`}>
-                        <button type="button" className="btn flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-green-600 text-white hover:bg-green-700">View Details</button>
+                    <Link to={`/tourist_card_details/${_id}`} className=''>
+                        <button type="button" className="btn btn-border p-0 transition-all ease-in-out duration-300 hover:bg-gradient-to-br hover:from-[#f6c459] hover:to-[#f871c0] hover:border-transparent hover:text-white flex items-center justify-center w-full font-semibold tracking-wide">View Details</button>
                     </Link>
                 </div>
             </div>
