dd30b83a83e195aaedb157c91f155b0d63ffe3d6
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-08-01 22:47:58 +0600

    Register page design changed

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,6 +5,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
 import { updateProfile } from "firebase/auth";
+import logo from "../../assets/Add a heading (1).png";
+import "./register.css";
 
 const Register = () => {
     const navigate = useNavigate();
@@ -33,13 +35,13 @@ const Register = () => {
         if (password.length < 6) {
             return setPasswordError('You should provide at least 6 character of password');
         }
-        else if(!/[A-Z]/.test(password)) {
+        else if (!/[A-Z]/.test(password)) {
             return setPasswordError('You should provide at least 1 character of Uppercase');
         }
-        else if(!/[a-z]/.test(password)) {
+        else if (!/[a-z]/.test(password)) {
             return setPasswordError('You should provide at least 1 character of Lowercase')
         }
-        else if(password !== confirmPassword) {
+        else if (password !== confirmPassword) {
             return setConfirmPasswordError('Confirm password must be same with the password');
         }
 
@@ -58,7 +60,7 @@ const Register = () => {
                         console.error(err.message);
                     })
 
-                if(crediential.user) {
+                if (crediential.user) {
                     toast.success('You have registered successfully');
                     setTimeout(() => {
                         navigate('/');
@@ -67,10 +69,10 @@ const Register = () => {
             })
             .catch(err => {
                 console.error(err.message);
-                if(err.message === "Firebase: Error (auth/email-already-in-use).") {
+                if (err.message === "Firebase: Error (auth/email-already-in-use).") {
                     toast.warn('You have already registered');
                 }
-                else if(err.message === "Firebase: Error (auth/network-request-failed).") {
+                else if (err.message === "Firebase: Error (auth/network-request-failed).") {
                     toast.error('Please check your internet connection');
                 }
             });
@@ -78,81 +80,83 @@ const Register = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
-                <div className="flex justify-center mb-8">
-                    <img src="https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png" alt="Logo" className="w-30 h-20" />
+            <div className="p-0 w-fit form-border mx-3">
+                <div className="w-auto md:w-[424px] m-6 rounded-lg">
+                    <div className="flex justify-center mb-8">
+                        <img src={logo} alt="Awesome Tourist Spots" className="w-30 h-20" />
+                    </div>
+                    <h1 className="text-2xl md:text-3xl font-semibold text-center text-gray-500 mb-8">Register Your Account</h1>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-4">
+                            <label htmlFor="name" className="block mb-2 text-sm text-gray-600">Your Name</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="w-full px-3 py-2 border border-[#f6c459] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f871c0]"
+                                placeholder="Enter your name"
+                                {...register("name", { required: true })}
+                            />
+                            {errors.name && <span className="text-red-600">This field is required</span>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block mb-2 text-sm text-gray-600">Your Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="w-full px-3 py-2 border border-[#f6c459] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f871c0]"
+                                placeholder="Enter your email"
+                                {...register("email", { required: true })}
+                            />
+                            {errors.email && <span className="text-red-600">This field is required</span>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="photo_url" className="block mb-2 text-sm text-gray-600">Your Photo url</label>
+                            <input
+                                type="text"
+                                id="photo_url"
+                                name="photo_url"
+                                className="w-full px-3 py-2 border border-[#f6c459] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f871c0]"
+                                placeholder="Enter your photo url"
+                                {...register("photo_url", { required: true })}
+                            />
+                            {errors.photo_url && <span className="text-red-600">This field is required</span>}
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block mb-2 text-sm text-gray-600">Your Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                className="w-full px-3 py-2 border border-[#f6c459] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f871c0]"
+                                placeholder="Enter your password"
+                                {...register("password", { required: true })}
+                            />
+                            {errors.password && <span className="text-red-600">This field is required</span>}
+                            {<span className="text-red-600">{passwordError}</span>}
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="confirmPassword" className="block mb-2 text-sm text-gray-600">Confirm Your Password</label>
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                className="w-full px-3 py-2 border border-[#f6c459] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#f871c0]"
+                                placeholder="Confirm your password"
+                                {...register("confirmPassword", { required: true })}
+                            />
+                            {errors.confirmPassword && <span className="text-red-600">This field is required</span>}
+                            {<span className="text-red-600">{confirmPasswordError}</span>}
+                        </div>
+                        <input type="submit" value={'Register now'} className="w-32 hover:cursor-pointer bg-gradient-to-br from-[#f6c459] to-[#f871c0] text-white py-2 rounded-lg mx-auto block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#f871c0] mb-2" />
+                    </form>
+                    <div className="text-center">
+                        <p className="text-sm">Already have an account? <Link to="/sign_in" className="text-[#f871c0] hover:underline">Sign in now</Link></p>
+                    </div>
                 </div>
-                <h1 className="text-2xl font-semibold text-center text-gray-500 mt-8 mb-6">Register Your Account</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mb-4">
-                        <label htmlFor="name" className="block mb-2 text-sm text-gray-600">Your Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Enter your name"
-                            {...register("name", { required: true })}
-                        />
-                        {errors.name && <span className="text-red-600">This field is required</span>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block mb-2 text-sm text-gray-600">Your Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Enter your email"
-                            {...register("email", { required: true })}
-                        />
-                         {errors.email && <span className="text-red-600">This field is required</span>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="photo_url" className="block mb-2 text-sm text-gray-600">Your Photo url</label>
-                        <input
-                            type="text"
-                            id="photo_url"
-                            name="photo_url"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Enter your photo url"
-                            {...register("photo_url", { required: true })}
-                        />
-                         {errors.photo_url && <span className="text-red-600">This field is required</span>}
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block mb-2 text-sm text-gray-600">Your Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Enter your password"
-                            {...register("password", { required: true })}
-                        />
-                        {errors.password && <span className="text-red-600">This field is required</span>}
-                        {<span className="text-red-600">{passwordError}</span>}
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="confirmPassword" className="block mb-2 text-sm text-gray-600">Confirm Your Password</label>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Confirm your password"
-                            {...register("confirmPassword", { required: true })}
-                        />
-                        {errors.confirmPassword && <span className="text-red-600">This field is required</span>}
-                        {<span className="text-red-600">{confirmPasswordError}</span>}
-                    </div>
-                    <button type="submit" className="w-32 bg-gradient-to-r from-cyan-400 to-cyan-600 text-white py-2 rounded-lg mx-auto block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 mb-2">Register now</button>
-                </form>
-                <div className="text-center">
-                    <p className="text-sm">Already have an account? <Link to="/sign_in" className="text-cyan-600 hover:underline">Sign in now</Link></p>
-                </div>
+                <ToastContainer />
             </div>
-            <ToastContainer />
         </div>
 
     );
--- src/SharedPage/Footer/Footer.jsx
+++ src/SharedPage/Footer/Footer.jsx
@@ -8,76 +8,76 @@ const Footer = () => {
     return (
         <footer className="w-full text-gray-700 bg-gray-100 footer-border mt-12">
             <div
-                className="container flex flex-col flex-wrap px-5 py-16 pb-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-                <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-24 lg:mx-0 md:text-left mb-10 lg:mb-0">
+                className="container flex flex-col flex-wrap px-6 lg:px-16 py-16 pb-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+                <div className="flex-shrink-0 w-64 mx-auto text-center xl:mx-0 md:text-left mb-10 lg:mb-0">
                     <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
                         <img src={webLogo} alt="Website logo" className="w-32 h-32 " />
                     </a>
                    <h1 className="mt-3 text-2xl font-semibold gradient-text-color">Awesome Tourist Spots</h1>
                 </div>
-                <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+                <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 lg:mt-6 xl:mt-0 md:text-left">
                     <div className="w-full px-4 lg:w-1/4 md:w-1/2">
                         <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Contact</h2>
-                        <nav className="mb-10 list-none">
+                        <nav className="mb-10 list-none gradient-text-link">
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Send a Message</a>
+                                <a className="text-gray-500 cursor-pointer">Send a Message</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Request a Quote</a>
+                                <a className="text-gray-500 cursor-pointer">Request a Quote</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">+123-456-7890</a>
+                                <a className="text-gray-500 cursor-pointer">+123-456-7890</a>
                             </li>
                         </nav>
                     </div>
                     <div className="w-full px-4 lg:w-1/4 md:w-1/2">
                         <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">About</h2>
-                        <nav className="mb-10 list-none">
+                        <nav className="mb-10 list-none gradient-text-link">
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Company</a>
+                                <a className="text-gray-500 cursor-pointer">Company</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Careers</a>
+                                <a className="text-gray-500 cursor-pointer">Careers</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Blog</a>
+                                <a className="text-gray-500 cursor-pointer">Blog</a>
                             </li>
                         </nav>
                     </div>
                     <div className="w-full px-4 lg:w-1/4 md:w-1/2">
                         <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Support</h2>
-                        <nav className="mb-10 list-none">
+                        <nav className="mb-10 list-none gradient-text-link">
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Contact Support</a>
+                                <a className="text-gray-500 cursor-pointer">Contact Support</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Help Resources</a>
+                                <a className="text-gray-500 cursor-pointer">Help Resources</a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900">Release Updates</a>
+                                <a className="text-gray-500 cursor-pointer">Release Updates</a>
                             </li>
                         </nav>
                     </div>
                     <div className="w-full px-4 lg:w-1/4 md:w-1/2">
                         <h2 className="mb-3 text-sm font-medium tracking-widest uppercase title-font">Social media links</h2>
-                        <nav className="mb-10 list-none mx-auto w-fit md:w-auto">
+                        <nav className="mb-10 list-none mx-auto w-fit md:w-auto gradient-text-link">
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900 flex gap-2"><FaSquareFacebook className="w-6 h-6 text-blue-500" /><span>Facebook</span></a>
+                                <a className="text-gray-500 cursor-pointer flex gap-2"><FaSquareFacebook className="w-6 h-6 text-blue-500" /><span>Facebook</span></a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900 flex gap-2"><BsTwitterX className="w-5 h-5 text-black" /><span>Twitter</span></a>
+                                <a className="text-gray-500 cursor-pointer flex gap-2"><BsTwitterX className="w-5 h-5 text-black" /><span>Twitter</span></a>
                             </li>
                             <li className="mt-3">
-                                <a className="text-gray-500 cursor-pointer hover:text-gray-900 flex gap-2"><SiInstagram className="w-6 h-6 text-black" />Instagram</a>
+                                <a className="text-gray-500 cursor-pointer flex gap-2"><SiInstagram className="w-6 h-6 text-black" />Instagram</a>
                             </li>
                         </nav>
                     </div>
                     
                 </div>
             </div>
-            <div className="bg-gradient-to-br from-[#f6c459] to-[#f871c0] rounded-b-[64px]">
-                <div className="container px-5 py-4 mx-auto">
-                    <p className="text-sm text-white capitalize text-center">© 2024 All rights reserved by Awesome Tourist Spots</p>
+            <div className="px-0 -mb-[1px]">
+                <div className="container w-screen py-8 lg:py-4 bg-gradient-to-br from-[#f6c459] to-[#f871c0]">
+                    <p className="text-sm text-white capitalize text-center w-56 md:w-72 lg:w-auto mx-auto">© 2024 All rights reserved by Awesome Tourist Spots</p>
                 </div>
             </div>
         </footer>
